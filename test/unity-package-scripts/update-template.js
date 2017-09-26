bdc120895f3626c54085926d2451b4e3295d62e8
larrykirschner <larrykirschner@gmail.com>
2017-09-26 12:19:54 -0700

    npm run template:update' - updates scripts and template files for an existing unity package

--- test/unity-package-scripts/update-template.js
+++ test/unity-package-scripts/update-template.js
@@ -3,35 +3,12 @@ const expect = require('chai').expect;
 const h = require('../test-helpers.js');
 const unpm = require('../../lib/unity-npm-utils');
 
+const updateTemplateBehaviour = require('../shared/update-package-template-behaviour.js')
 
-describe.skip("'npm run template:update' - updates scripts and template files for an existing unity package", () => {
-    var pkgPath = null;
-
-    const pkgNameFoo = "my-pkg-foo";
-    var pkgBefore = null;
-
-    beforeEach(function(done) {
-        this.timeout(10000);
-
-        h.installUnityPackageTemplateToTemp({
-            package_name: pkgNameFoo
-        }, (installErr, tmpInstallPath) => {
-            if (installErr) {
-                return done(installErr);
-            }
-
-            pkgPath = tmpInstallPath;
-
-            pkgBefore = h.readPackageSync(pkgPath);
-
-            h.runPkgCmd('npm run template:update', pkgPath, (cmdErr) => {
-                return done(cmdErr);
-            });
-        });
+describe.only("'npm run template:update' - updates scripts and template files for an existing unity package", () => {
+    updateTemplateBehaviour((opts) => {
+        return h.runPkgCmd('npm run template:update', opts.package_path);
+    }, {
+        install_required: true
     });
-
-    it("appends all template scripts to main package scripts", function(done) {
-        done(new Error('not implemented'))
-    });
-
 });
