854aec217ddc71d7db18a246bcef5604a83a335c
larrykirschner <larrykirschner@gmail.com>
2017-10-09 17:11:16 -0700

    installTemplate test now async

--- test/module/increment-package-version.js
+++ test/module/increment-package-version.js
@@ -1,15 +1,7 @@
 const expect = require('chai').expect;
-const path = require('path');
-const fs = require('fs');
-const tmp = require('tmp');
-const spawn = require('child_process').spawn;
-const mlog = require('mocha-logger');
 
 const h = require('../test-helpers.js');
 const unpm = require('../../lib/unity-npm-utils');
-
-tmp.setGracefulCleanup();
-
 
 describe("incrementPackageVersion - increments the version for package", () => {
     var pkgPath = null;
--- test/module/unity-package/install-template.js
+++ test/module/unity-package/install-template.js
@@ -1,36 +1,23 @@
 const expect = require('chai').expect;
+const fs = require('fs-extra-promise');
 const path = require('path');
-const fs = require('fs');
-const tmp = require('tmp');
-const spawn = require('child_process').spawn;
-const mlog = require('mocha-logger');
 
 const h = require('../../test-helpers.js');
 const unpm = require('../../../lib/unity-npm-utils');
 
-
-tmp.setGracefulCleanup();
-
 describe("unityPackage.installTemplate - installs a the unity-package template to an empty directory", () => {
     var pkgPath = null;
 
-    before(function(done) {
+    before(async function() {
         this.timeout(10000);
 
-        h.installUnityPackageTemplateToTemp((installErr, tmpInstallPath) => {
-            if (installErr) {
-                return done(installErr);
-            }
-
-            pkgPath = tmpInstallPath;
-            done();
-        });
+        pkgPath = await h.installUnityPackageTemplateToTemp();
     });
 
-    it("installs template files", () => {
+    it("installs template files", async () => {
         const packageJsonPath = path.join(pkgPath, 'package.json');
 
-        expect(fs.existsSync(packageJsonPath),
+        expect(await fs.existsAsync(packageJsonPath),
             `package.json should exist in install path ${pkgPath}`).to.equal(true);
     });
 
