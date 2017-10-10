189658f1d5a77c2c9b58113549ba19ee25476121
larrykirschner <larrykirschner@gmail.com>
2017-10-09 17:35:28 -0700

    setPackageName works with async/await

--- test/module/unity-package/set-package-name.js
+++ test/module/unity-package/set-package-name.js
@@ -1,184 +1,125 @@
 const expect = require('chai').expect;
 const path = require('path');
-const fs = require('fs');
-const tmp = require('tmp');
-const mlog = require('mocha-logger');
+const fs = require('fs-extra-promise');
 
 const h = require('../../test-helpers.js');
 const unpm = require('../../../lib/unity-npm-utils');
 
-    describe("unityPackage.setPackageName - sets package.name and updates all name-dependent aspects of a unity package", () => {
-        var pkgPath = null;
+describe("unityPackage.setPackageName - sets package.name and updates all name-dependent aspects of a unity package", () => {
+    var pkgPath = null;
 
-        beforeEach(function(done) {
-            this.timeout(10000);
+    beforeEach(async function() {
+        this.timeout(10000);
 
-            h.installUnityPackageTemplateToTemp((installErr, tmpInstallPath) => {
-                if (installErr) {
-                    return done(installErr);
-                }
-
-                pkgPath = tmpInstallPath;
-
-                done();
-            });
-        });
-
-        it("writes new name [and option scope] to package.json", function(done) {
-            this.timeout(10000);
-
-            const newPkgName = 'my-new-pkg-name';
-            const newPkgScope = 'my-pkg-scope'
-
-            unpm.unityPackage.setPackageName(pkgPath, {
-                package_name: newPkgName,
-                package_scope: newPkgScope
-            })
-            .then(pkgPath => {
-
-                const pkgWritten = h.readPackageSync(pkgPath);
-                expect(pkgWritten.name, 'should have written name').to.equal(newPkgName);
-                expect(pkgWritten.config.scope, 'should have written scope as a config property').to.equal(newPkgScope);
-                done();
-            })
-            .catch(e => {
-                console.log(new Error().stack);
-                done(e);
-            });
-        });
-
-        it("forces package name and scope to cannonical lowercase and dash-delimited", function(done) {
-            this.timeout(10000);
-
-            const newPkgName = 'My New Pkg Name';
-            const newPkgScope = 'My_Pkg Scope ';
-
-            const cannonicalPkgName = 'my-new-pkg-name';
-            const cannonicalPkgScope = 'my-pkg-scope'
-
-            unpm.unityPackage.setPackageName(pkgPath, {
-                package_name: newPkgName,
-                package_scope: newPkgScope
-            })
-            .then(pkgPath => {
-
-                const pkgWritten = h.readPackageSync(pkgPath);
-                expect(pkgWritten.name,
-                    'should have translated name to cannonical lowercase and dash-delimited form'
-                ).to.equal(cannonicalPkgName);
-
-                expect(pkgWritten.config.scope,
-                    'should have translated scope to cannonical lowercase and dash-delimited form'
-                ).to.equal(cannonicalPkgScope);
-
-                done();
-            })
-            .catch(e => {
-                console.log(new Error().stack);
-                done(e);
-            });
-        });
-
-        it("sets a github urls for repo, issues, and homepage by default when package scope is set", function(done) {
-            this.timeout(10000);
-
-            const newPkgName = 'My New Pkg Name';
-            const newPkgScope = 'My_Pkg Scope ';
-
-            const cannonicalPkgName = 'my-new-pkg-name';
-            const cannonicalPkgScope = 'my-pkg-scope'
-
-            unpm.unityPackage.setPackageName(pkgPath, {
-                package_name: newPkgName,
-                package_scope: newPkgScope
-            })
-            .then(pkgPath => {
-
-                const pkgWritten = h.readPackageSync(pkgPath);
-                expect(pkgWritten.repository.type, 'should have written repository type as git').to.equal('git');
-                expect(pkgWritten.repository.url, 'should have written repository type as github url').to.equal(
-                    `git+https://github.com/${cannonicalPkgScope}/${cannonicalPkgName}.git`
-                );
-
-                expect(pkgWritten.bugs.url, 'should have written issues github bugs url').to.equal(
-                    `https://github.com/${cannonicalPkgScope}/${cannonicalPkgName}/issues`
-                );
-
-                expect(pkgWritten.homepage, 'should have written homepage github homepage url').to.equal(
-                    `https://github.com/${cannonicalPkgScope}/${cannonicalPkgName}`
-                );
-
-                done();
-            })
-            .catch(e => {
-                console.log(new Error().stack);
-                done(e);
-            });
-        });
-
-        it("adds a folder under src with the new package name if none exists", function(done) {
-            this.timeout(10000);
-
-            const newPkgName = 'my-new-name-for-this-pkg';
-
-            unpm.unityPackage.setPackageName(pkgPath, {
-                package_name: newPkgName,
-                verbose: false
-            }, (err, pkgAfter) => {
-                if (err) {
-                    return done(err);
-                }
-
-                const pkgSrcPath = path.join(pkgPath, 'src', newPkgName);
-                fs.stat(pkgSrcPath, (statErr, stats) => {
-                    if (statErr) {
-                        return done(statErr);
-                    }
-                    expect(stats.isDirectory()).to.equal(true);
-                    return done();
-                });
-            });
-        });
-
-        it("renames the existing (single) folder under (package_root)/src with the new package name", function(done) {
-            this.timeout(10000);
-
-            const newPkgName_1 = 'my-new-name-for-this-pkg-1';
-            const newPkgName_2 = 'my-new-name-for-this-pkg-2';
-
-            unpm.unityPackage.setPackageName(pkgPath, {
-                package_name: newPkgName_1,
-                verbose: false
-            }, (err) => {
-                if (err) {
-                    return done(err);
-                }
-
-                fs.stat(path.join(pkgPath, 'src', newPkgName_1), (statErr, stats) => {
-                    if (statErr) {
-                        return done(statErr);
-                    }
-
-                    expect(stats.isDirectory()).to.equal(true);
-
-                    unpm.unityPackage.setPackageName(pkgPath, {
-                        package_name: newPkgName_2,
-                        verbose: false
-                    }, (err2) => {
-                        if (err2) {
-                            return done(err2);
-                        }
-
-                        fs.stat(path.join(pkgPath, 'src', newPkgName_2), (statErr_2, stats_2) => {
-                            if (statErr_2) {
-                                return done(statErr_2);
-                            }
-                            expect(stats_2.isDirectory()).to.equal(true);
-                            return done();
-                        });
-                    });
-                });
-            });
-        });
-
+        pkgPath = await h.installUnityPackageTemplateToTemp();
     });
+
+    it("writes new name [and option scope] to package.json", async function() {
+        this.timeout(10000);
+
+        const newPkgName = 'my-new-pkg-name';
+        const newPkgScope = 'my-pkg-scope'
+
+        await unpm.unityPackage.setPackageName(pkgPath, {
+            package_name: newPkgName,
+            package_scope: newPkgScope
+        });
+
+        const pkgWritten = h.readPackageSync(pkgPath);
+
+        expect(pkgWritten.name, 'should have written name').to.equal(newPkgName);
+        expect(pkgWritten.config.scope, 'should have written scope as a config property').to.equal(newPkgScope);
+    });
+
+    it("forces package name and scope to cannonical lowercase and dash-delimited", async function() {
+        this.timeout(10000);
+
+        const newPkgName = 'My New Pkg Name';
+        const newPkgScope = 'My_Pkg Scope ';
+
+        const cannonicalPkgName = 'my-new-pkg-name';
+        const cannonicalPkgScope = 'my-pkg-scope'
+
+        await unpm.unityPackage.setPackageName(pkgPath, {
+            package_name: newPkgName,
+            package_scope: newPkgScope
+        });
+
+        const pkgWritten = h.readPackageSync(pkgPath);
+        expect(pkgWritten.name,
+            'should have translated name to cannonical lowercase and dash-delimited form'
+        ).to.equal(cannonicalPkgName);
+
+        expect(pkgWritten.config.scope,
+            'should have translated scope to cannonical lowercase and dash-delimited form'
+        ).to.equal(cannonicalPkgScope);
+    });
+
+    it("sets a github urls for repo, issues, and homepage by default when package scope is set", async function() {
+        this.timeout(10000);
+
+        const newPkgName = 'My New Pkg Name';
+        const newPkgScope = 'My_Pkg Scope ';
+
+        const cannonicalPkgName = 'my-new-pkg-name';
+        const cannonicalPkgScope = 'my-pkg-scope'
+
+        await unpm.unityPackage.setPackageName(pkgPath, {
+            package_name: newPkgName,
+            package_scope: newPkgScope
+        })
+
+        const pkgWritten = h.readPackageSync(pkgPath);
+        expect(pkgWritten.repository.type, 'should have written repository type as git').to.equal('git');
+        expect(pkgWritten.repository.url, 'should have written repository type as github url').to.equal(
+            `git+https://github.com/${cannonicalPkgScope}/${cannonicalPkgName}.git`
+        );
+
+        expect(pkgWritten.bugs.url, 'should have written issues github bugs url').to.equal(
+            `https://github.com/${cannonicalPkgScope}/${cannonicalPkgName}/issues`
+        );
+
+        expect(pkgWritten.homepage, 'should have written homepage github homepage url').to.equal(
+            `https://github.com/${cannonicalPkgScope}/${cannonicalPkgName}`
+        );
+    });
+
+    it("adds a folder under src with the new package name if none exists", async function() {
+        this.timeout(10000);
+
+        const newPkgName = 'my-new-name-for-this-pkg';
+
+        const pkgAfter = await unpm.unityPackage.setPackageName(pkgPath, {
+            package_name: newPkgName,
+            verbose: false
+        });
+
+        const pkgSrcPath = path.join(pkgPath, 'src', newPkgName);
+        const stats = await fs.statAsync(pkgSrcPath);
+        expect(stats.isDirectory()).to.equal(true);
+    });
+
+    it("renames the existing (single) folder under (package_root)/src with the new package name", async function() {
+        this.timeout(10000);
+
+        const newPkgName_1 = 'my-new-name-for-this-pkg-1';
+        const newPkgName_2 = 'my-new-name-for-this-pkg-2';
+
+        await unpm.unityPackage.setPackageName(pkgPath, {
+            package_name: newPkgName_1,
+            verbose: false
+        });
+
+        const stats = await fs.statAsync(path.join(pkgPath, 'src', newPkgName_1));
+
+        expect(stats.isDirectory()).to.equal(true);
+
+        await unpm.unityPackage.setPackageName(pkgPath, {
+            package_name: newPkgName_2,
+            verbose: false
+        });
+
+        const stats_2 = await fs.statAsync(path.join(pkgPath, 'src', newPkgName_2));
+        expect(stats_2.isDirectory()).to.equal(true);
+    });
+});
