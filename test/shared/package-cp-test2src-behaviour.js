15915ed7d19f91b6a6138888501479417fac299b
larrykirschner <larrykirschner@gmail.com>
2017-10-09 18:05:08 -0700

    copy|overwrite test2src test passes with async calls

--- test/shared/package-cp-test2src-behaviour.js
+++ test/shared/package-cp-test2src-behaviour.js
@@ -46,29 +46,20 @@ const copy2SrcBehaviour = (copy2Src, options) => {
         }
     ];
 
-    beforeEach(function(done) {
+    beforeEach(async function() {
         this.timeout(180000);
 
-        h.installUnityPackageTemplateToTemp({
+        pkgPath = await h.installUnityPackageTemplateToTemp({
             package_name: pkgName,
             run_npm_install: true
-        }, (installErr, tmpInstallPath) => {
-            if (installErr) {
-                return done(installErr);
-            }
+        })
 
-            pkgPath = tmpInstallPath;
+        await unpm.unityPackage.addSrcFiles(pkgPath, srcFilesBefore)
 
-            unpm.unityPackage.addSrcFiles(pkgPath, srcFilesBefore)
-            .then(srcFilesAdded => {
-                h.runPkgCmd('npm run install:test', pkgPath)
-                .then(installTestDone => done())
-                .catch(e => done(e));
-            })
-        });
+        await h.runPkgCmd('npm run install:test', pkgPath)
     });
 
-    it('adds new files created in the Unity project to pkg src and overwrites existing pkg-src files with changes made in the Unity project', function(done) {
+    it('adds new files created in the Unity project to pkg src and overwrites existing pkg-src files with changes made in the Unity project', async function() {
 
         this.timeout(10000);
 
@@ -80,87 +71,65 @@ const copy2SrcBehaviour = (copy2Src, options) => {
         unityFiles[0].content = 'public class Foo { // added code }';
         unityFiles[1].content = 'public class Bar { // added code here too }';
 
-        writeFilesToUnityThenCopy2Pkg(pkgPath, pkgName, copy2Src, unityFiles).
-        then(success => done())
-        .catch(e => done(e))
+        await writeFilesToUnityThenCopy2Pkg(pkgPath, pkgName, copy2Src, unityFiles)
     })
 
     const expectDeletes = options.expect_deletes_from_package;
     it(expectDeletes?
         'deletes package src files not present in the Unity test source':
-        'does NOT delete package src files not present in the Unity test source', function(done) {
+        'does NOT delete package src files not present in the Unity test source', async function() {
 
         this.timeout(10000);
 
         const unityFiles = srcFilesBefore.slice(0, 1);
         const deleteUnityFiles = srcFilesBefore.slice(1);
 
-        writeFilesToUnityThenCopy2Pkg(pkgPath, pkgName, copy2Src, unityFiles, deleteUnityFiles, expectDeletes).
-        then(allSuccess => done())
-        .catch(e => done(e))
+        await writeFilesToUnityThenCopy2Pkg(pkgPath, pkgName, copy2Src, unityFiles, deleteUnityFiles, expectDeletes)
     });
 }
 
-const writeFilesToUnityThenCopy2Pkg = (pkgPath, pkgName, copy2Src, unityFiles, deleteUnityFiles, expectDeletes) => {
-    return new Promise((doneResolve, doneReject) => {
-        const unitySrcRoot = path.join(pkgPath, 'test', 'Assets', 'Plugins', 'packages', pkgName);
+const writeFilesToUnityThenCopy2Pkg = async (pkgPath, pkgName, copy2Src, unityFiles, deleteUnityFiles, expectDeletes) => {
 
-        const unityChanges = unityFiles.map(f => {
-            return new Promise((resolve, reject) => {
-                fs.writeFileAsync(path.join(unitySrcRoot, f.name), f.content)
-                .then(wroteOne => resolve())
+    const unitySrcRoot = path.join(pkgPath, 'test', 'Assets', 'Plugins', 'packages', pkgName);
+
+    const unityChanges = unityFiles.map(async f =>
+        await fs.writeFileAsync(path.join(unitySrcRoot, f.name), f.content)
+    )
+
+    if(deleteUnityFiles) {
+        deleteUnityFiles.forEach(async f =>
+            unityChanges.push(await fs.unlinkAsync(path.join(unitySrcRoot, f.name)))
+        )
+    }
+
+    await Promise.all(unityChanges)
+
+    await copy2Src({ package_path: pkgPath });
+
+    const pkgSrcRoot = path.join(pkgPath, 'src', pkgName);
+
+    const pkgChanges = unityFiles.map(async f =>
+        await fs.readFileAsync(path.join(pkgSrcRoot, f.name), 'utf8')
+    );
+
+    if(deleteUnityFiles) {
+        deleteUnityFiles.forEach(f => {
+            pkgChanges.push(new Promise((resolve, reject) => {
+                const fpath = path.join(pkgSrcRoot, f.name);
+                fs.existsAsync(path.join(pkgSrcRoot, f.name))
+                .then(deletedFileStillExistsInPkg => {
+                    expect(deletedFileStillExistsInPkg, expectDeletes?
+                        `${fpath} should be deleted from package source because it is not present in unity test source`:
+                        `${fpath} should NOT be deleted because overwrite option isn't set for this copy`
+                    ).to.not.equal(expectDeletes);
+                    resolve();
+                })
                 .catch(e => reject(e))
-            })
+            }));
         });
+    }
 
-        if(deleteUnityFiles) {
-            deleteUnityFiles.forEach(f => {
-                unityChanges.push(new Promise((resolve, reject) => {
-                    fs.unlinkAsync(path.join(unitySrcRoot, f.name))
-                    .then(deletedOne => resolve())
-                    .catch(e => reject(e))
-                }));
-            });
-        }
-
-        Promise.all(unityChanges)
-        .then(unitySrcUpdated => copy2Src({ package_path: pkgPath }))
-        .then(copied => {
-            const pkgSrcRoot = path.join(pkgPath, 'src', pkgName);
-
-            const pkgChanges = unityFiles.map(f => {
-                return new Promise((resolve, reject) => {
-                    fs.readFileAsync(path.join(pkgSrcRoot, f.name), 'utf8')
-                    .then(content => {
-                        expect(content).to.equal(f.content);
-                        resolve();
-                    })
-                    .catch(e => reject(e))
-                });
-            });
-
-            if(deleteUnityFiles) {
-                deleteUnityFiles.forEach(f => {
-                    pkgChanges.push(new Promise((resolve, reject) => {
-                        const fpath = path.join(pkgSrcRoot, f.name);
-                        fs.existsAsync(path.join(pkgSrcRoot, f.name))
-                        .then(deletedFileStillExistsInPkg => {
-                            expect(deletedFileStillExistsInPkg, expectDeletes?
-                                `${fpath} should be deleted from package source because it is not present in unity test source`:
-                                `${fpath} should NOT be deleted because overwrite option isn't set for this copy`
-                            ).to.not.equal(expectDeletes);
-                            resolve();
-                        })
-                        .catch(e => reject(e))
-                    }));
-                });
-            }
-
-            return Promise.all(pkgChanges)
-        })
-        .then(allSuccess => doneResolve())
-        .catch(e => doneReject(e))
-    });
+    await Promise.all(pkgChanges)
 }
 
 
