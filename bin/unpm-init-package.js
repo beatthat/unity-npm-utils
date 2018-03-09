7e63e1d69883933a9177114a18c296f1f97d3c64
Larry Kirschner <larrykirschner@gmail.com>
2018-03-09 07:50:54 -0800

    unityPackage.installTemplate - installs a the unity-package template to an empty directory     ✓ sets package name to install dir name by default

--- bin/unpm-init-package.js
+++ bin/unpm-init-package.js
@@ -7,7 +7,7 @@ const thisDir = __dirname;
 const unpm = require(path.join(__dirname, '..', 'lib', 'unity-npm-utils.js'));
 
 opts
-  .option('-n, --package-name [name]', 'set the package name')
+  .option('-n, --package-name [name]', 'set the package name (default is the directory name of the install path)')
   .option('-p, --install-path [path]', 'set the install path for the package (default is cwd)')
   .option('-v, --verbose', 'log verbose info to console')
   .parse(process.argv);
--- test/module/unity-package/install-template.js
+++ test/module/unity-package/install-template.js
@@ -1,29 +1,34 @@
-const expect = require('chai').expect;
-const fs = require('fs-extra-promise');
-const path = require('path');
+const expect = require('chai').expect
+const fs = require('fs-extra-promise')
+const path = require('path')
 
-const h = require('../../test-helpers.js');
-const unpm = require('../../../lib/unity-npm-utils');
+const h = require('../../test-helpers.js')
+const unpm = require('../../../lib/unity-npm-utils')
 
 describe("unityPackage.installTemplate - installs a the unity-package template to an empty directory", () => {
-    var pkgPath = null;
+    var pkgPath = null
 
     before(async function() {
-        this.timeout(10000);
+        this.timeout(10000)
 
-        pkgPath = await h.installUnityPackageTemplateToTemp();
-    });
+        pkgPath = await h.installUnityPackageTemplateToTemp()
+    })
 
     it("installs template files", async () => {
-        const packageJsonPath = path.join(pkgPath, 'package.json');
+        const packageJsonPath = path.join(pkgPath, 'package.json')
 
         expect(await fs.existsAsync(packageJsonPath),
-            `package.json should exist in install path ${pkgPath}`).to.equal(true);
-    });
+            `package.json should exist in install path ${pkgPath}`).to.equal(true)
+    })
 
     it("sets initial version to 0.0.1", () => {
-        const pkg = h.readPackageSync(pkgPath);
-        expect(pkg.version).to.equal('0.0.1');
-    });
+        const pkg = h.readPackageSync(pkgPath)
+        expect(pkg.version).to.equal('0.0.1')
+    })
 
-});
+    it("sets package name to install dir name by default", () => {
+        const pkg = h.readPackageSync(pkgPath)
+        expect(pkg.name).to.equal(path.basename(pkgPath))
+    })
+
+})
