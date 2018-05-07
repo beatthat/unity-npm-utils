c5c73b033fcfab2200823325cd3b75939c30fc80
larrykirschner <larrykirschner@gmail.com>
2018-05-06 21:51:32 -0700

    added more ability to find unity path from package name for packages with scope defined in unpm-packages.json

---
+++ test/module/unity-project/find-unity-path-for-package.js
@@ -0,0 +1,63 @@
+const expect = require('chai').expect
+const fs = require('fs-extra-promise')
+const path = require('path')
+
+const h = require('../../test-helpers.js')
+const unpm = require('../../../lib/unity-npm-utils')
+const appRoot = require('app-root-path').path
+
+const VERBOSE = false
+
+describe.only("unityProject.findUnityPathForPackage", () => {
+
+
+    it("- can guess unity path when a package's scope is defined in unpm-package.json", async function() {
+      this.timeout(300000)
+
+
+      //////////////////////////////////////////////////////////////////////
+      // first let's create a test package with unity-npm-utils installed...
+      //////////////////////////////////////////////////////////////////////
+
+      const testProjPath = await h.installLocalUnpmToPackage()
+
+      // set up a unpm-packages.json with some scope definition
+      await unpm.transformJson({
+          path: testProjPath,
+          file_name: 'unpm-packages.json',
+          json_default: {
+              scopes: {
+                beatthat: {
+                    name: 'beatthat',
+                    repository: {
+                        type: 'git',
+                        url: 'git+https://github.com/beatthat/{package_name}.git'
+                    },
+                    install: {
+                        path: path.join('Assets', 'Plugins', 'packages', 'beatthat', '{package_name}')
+                    }
+                }
+              }
+          },
+          transformAsync: async (p) => {
+              return p; // will just write json_default
+          }
+      })
+
+      const testPkgName = "property-interfaces"
+      const testPkgScope = "beatthat"
+      const testFullPkgFullName = `${testPkgScope}/${testPkgName}`
+
+      await h.runPkgCmdAsync('npm install --save ' + testFullPkgFullName, testProjPath)
+
+      const unityPath = await unpm.unityProject.findUnityPathForPackage(testPkgName, {
+        project_root: testProjPath,
+        verbose: VERBOSE
+      })
+
+      expect(unityPath).to.equal(
+          path.join(testProjPath, 'Assets', 'Plugins', 'packages', 'beatthat', testPkgName)
+      )
+    })
+
+})
