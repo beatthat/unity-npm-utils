64866b70f24a5503dbfc3fb84f69fd323133ef16
larrykirschner <larrykirschner@gmail.com>
2018-05-13 16:50:08 -0700

    moved util function out from test test/module/unity-project/install-package-to-unity.js to test/test-helpers.js

--- test/module/unity-project/install-package-to-unity.js
+++ test/module/unity-project/install-package-to-unity.js
@@ -2,72 +2,13 @@ const expect = require('chai').expect
 const fs = require('fs-extra-promise')
 const path = require('path')
 
-const h = require('../../test-helpers.js')
+const h = require('../../test-helpers')
 const unpm = require('../../../lib/unity-npm-utils')
 const appRoot = require('app-root-path').path
 
 const VERBOSE = true
 
-
-const npmInstallPackageWithIgnoreScripts = async function(pkgName, pkgScope)
-{
-    const unpmPkg = await unpm.readPackage(appRoot)
-
-    expect(unpmPkg.name).to.equal('unity-npm-utils')
-
-    //////////////////////////////////////////////////////////////////////
-    // first let's create a test package with unity-npm-utils installed...
-    //////////////////////////////////////////////////////////////////////
-
-    const testProjPath = await h.installLocalUnpmToPackage()
-
-    expect(
-      fs.existsSync(path.join(testProjPath, 'package.json')),
-      'test project should be installed at root ' + testProjPath
-    ).to.equal(true)
-
-    var testProj = await unpm.readPackage(testProjPath)
-
-    expect(
-      testProj.dependencies['unity-npm-utils']
-    ).to.exist
-
-    ////////////////////////////////////////////////////////////////////
-    // Now let's install a random unity package ('beatthat/properties' for this example)
-    // This is the package we will test against further down
-    ////////////////////////////////////////////////////////////////////
-
-    const testPkgFullName = `${pkgScope}/${pkgName}`
-
-    await h.runPkgCmdAsync('npm install --save --ignore-scripts ' + testPkgFullName, testProjPath)
-
-    testProj = await unpm.readPackage(testProjPath)
-
-    const testPkgInstallPath = path.join(testProjPath, 'Assets', 'Plugins', 'packages', pkgScope, pkgName)
-
-    expect(
-      await fs.existsAsync(testPkgInstallPath),
-      "(having installed package with ignore-scripts) the package should NOT yet be installed to unity"
-    ).to.equal(false)
-
-    const testPkgSamplesPath = path.join(testProjPath, 'Assets', 'Samples', 'packages', pkgScope, pkgName)
-
-    expect(
-      await fs.existsAsync(testPkgSamplesPath),
-      "(having installed package with ignore-scripts) the package's Samples should NOT yet be installed to unity"
-    ).to.equal(false)
-
-    return {
-        test_project_path: testProjPath,
-        test_package_name: pkgName,
-        test_package_scope: pkgScope,
-        test_package_full_name: testPkgFullName,
-        test_package_expected_unity_install_path: testPkgInstallPath,
-        test_package_expected_unity_samples_path: testPkgSamplesPath
-    }
-}
-
-describe("unityProject.installPackageToUnity", () => {
+describe.only("unityProject.installPackageToUnity", () => {
 
     /**
      * For a package that's already (npm) installed to node_modules,
@@ -77,7 +18,7 @@ describe("unityProject.installPackageToUnity", () => {
     it("installs code and samples from a package (already installed to node_modules) to unity Assets", async function() {
       this.timeout(300000)
 
-      const pkgInfo = await npmInstallPackageWithIgnoreScripts("placements", "beatthat")
+      const pkgInfo = await h.npmInstallPackageWithIgnoreScripts("placements", "beatthat")
 
       await unpm.unityProject.installPackageToUnity(pkgInfo.test_package_name, {
         project_root: pkgInfo.test_project_path,
@@ -98,7 +39,7 @@ describe("unityProject.installPackageToUnity", () => {
     it("does NOT create a Samples directory for the package in Unity if there are no samples in the package", async function() {
         this.timeout(300000)
 
-        const pkgInfo = await npmInstallPackageWithIgnoreScripts("placements", "beatthat")
+        const pkgInfo = await h.npmInstallPackageWithIgnoreScripts("placements", "beatthat")
 
         // get rid of the Samples directory from the package,
         // and then confirm that samples directory
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -9,9 +9,9 @@ const dateFormat = require('dateformat')
 
 const appRoot = require('app-root-path').path
 
-const unpm = require('../lib/unity-npm-utils');
+const unpm = require('../lib/unity-npm-utils')
 
-tmp.setGracefulCleanup();
+tmp.setGracefulCleanup()
 
 /**
  * @private
@@ -20,12 +20,12 @@ const _cmdToLogFileName = (cmd) => {
     return dateFormat(new Date(), 'yyyymmdd-hhMMss') + '-'
     + cmd.split(' ').reduce((acc, cur, i) => {
         return i < 3 && String(cur).match(/^[a-zA-Z0-9]+.*/)?
-            (acc? acc+'_': '') + cur.replace(/[^a-zA-Z0-9]/g, '-').substring(0, 20): acc;
-    }) + '.log';
+            (acc? acc+'_': '') + cur.replace(/[^a-zA-Z0-9]/g, '-').substring(0, 20): acc
+    }) + '.log'
 }
 
-mlog.styles.pending.prefix = mcoloring('pending', '        ...');
-mlog.styles.log.prefix = mcoloring('light', '        -');
+mlog.styles.pending.prefix = mcoloring('pending', '        ...')
+mlog.styles.log.prefix = mcoloring('light', '        -')
 
 /**
  * Run a package shell command (with cwd set to the package room),
@@ -40,30 +40,30 @@ const runPkgCmdAsync = async (cmd, pkgPath) => {
         const cmdProc = spawn(cmd, {
             shell: true,
             cwd: pkgPath
-        });
+        })
 
-        const log = path.join(pkgPath, _cmdToLogFileName(cmd));
+        const log = path.join(pkgPath, _cmdToLogFileName(cmd))
 
         logStream = fs.createWriteStream(log, {
             flags: 'a'
-        });
+        })
 
-        mlog.pending(`running '${cmd}'...`);
-        mlog.pending(`view logs at ${log}`);
-        mlog.pending('this may take a while...');
+        mlog.pending(`running '${cmd}'...`)
+        mlog.pending(`view logs at ${log}`)
+        mlog.pending('this may take a while...')
 
-        cmdProc.stdout.pipe(logStream);
-        cmdProc.stderr.pipe(logStream);
+        cmdProc.stdout.pipe(logStream)
+        cmdProc.stderr.pipe(logStream)
 
         cmdProc.on('exit', (code, signal) => {
             if (code !== 0 || signal) {
-                return reject(new Error(`${cmd} failed with code ${code} and signal ${signal}`));
+                return reject(new Error(`${cmd} failed with code ${code} and signal ${signal}`))
             }
-            return resolve();
-        });
-    });
+            return resolve()
+        })
+    })
 
-    return promise;
+    return promise
 }
 
 /**
@@ -80,31 +80,31 @@ const runPkgCmd = (cmd, pkgPath, callback) => {
         const cmdProc = spawn(cmd, {
             shell: true,
             cwd: pkgPath
-        });
+        })
 
-        const log = path.join(pkgPath, _cmdToLogFileName(cmd));
+        const log = path.join(pkgPath, _cmdToLogFileName(cmd))
 
         logStream = fs.createWriteStream(log, {
             flags: 'a'
-        });
+        })
 
-        mlog.pending(`running '${cmd}'...`);
-        mlog.pending(`view logs at ${log}`);
-        mlog.pending('this may take a while...');
+        mlog.pending(`running '${cmd}'...`)
+        mlog.pending(`view logs at ${log}`)
+        mlog.pending('this may take a while...')
 
-        cmdProc.stdout.pipe(logStream);
-        cmdProc.stderr.pipe(logStream);
+        cmdProc.stdout.pipe(logStream)
+        cmdProc.stderr.pipe(logStream)
 
         cmdProc.on('exit', (code, signal) => {
             if (code !== 0 || signal) {
-                return reject(new Error(`${cmd} failed with code ${code} and signal ${signal}`));
+                return reject(new Error(`${cmd} failed with code ${code} and signal ${signal}`))
             }
-            return resolve();
-        });
-    });
+            return resolve()
+        })
+    })
 
-    if (!callback) { return promise; }
-    promise.then(pr => callback(null, pr)).catch(pe => callback(pe));
+    if (!callback) { return promise }
+    promise.then(pr => callback(null, pr)).catch(pe => callback(pe))
 }
 
 /**
@@ -120,39 +120,39 @@ const runBinCmd = (cmd, callback) => {
 
     const promise = new Promise((resolve, reject) => {
 
-        const pkgPath = path.join(__dirname, '..');
+        const pkgPath = path.join(__dirname, '..')
 
-        const cmdParts = cmd.split(' ');
+        const cmdParts = cmd.split(' ')
 
-        const cmdFull = `node ${path.join(pkgPath, 'bin', cmdParts[0])} ${[...cmdParts.slice(1)].join(' ')}`;
+        const cmdFull = `node ${path.join(pkgPath, 'bin', cmdParts[0])} ${[...cmdParts.slice(1)].join(' ')}`
 
         const cmdProc = spawn(`${cmdFull}`, {
             shell: true
-        });
+        })
 
-        const log = path.join(pkgPath, _cmdToLogFileName(cmd));
+        const log = path.join(pkgPath, _cmdToLogFileName(cmd))
 
         logStream = fs.createWriteStream(log, {
             flags: 'a'
-        });
+        })
 
-        mlog.pending(`running 'node ${cmd}'...`);
-        mlog.pending(`logging to ${log}`);
+        mlog.pending(`running 'node ${cmd}'...`)
+        mlog.pending(`logging to ${log}`)
 
-        cmdProc.stdout.pipe(logStream);
-        cmdProc.stderr.pipe(logStream);
+        cmdProc.stdout.pipe(logStream)
+        cmdProc.stderr.pipe(logStream)
 
         cmdProc.on('exit', (code, signal) => {
             if (code !== 0 || signal) {
-                return reject(new Error(`${cmd} failed with code ${code} and signal ${signal}`));
+                return reject(new Error(`${cmd} failed with code ${code} and signal ${signal}`))
             }
 
-            return resolve();
-        });
-    });
+            return resolve()
+        })
+    })
 
-    if (!callback) { return promise; }
-    promise.then(pr => callback(null, pr)).catch(pe => callback(pe));
+    if (!callback) { return promise }
+    promise.then(pr => callback(null, pr)).catch(pe => callback(pe))
 }
 
 /**
@@ -162,7 +162,7 @@ const runBinCmd = (cmd, callback) => {
  * @returns {string} path of newly created temp directory
  */
 const createTmpPackageDir = async (opts) => {
-  opts = opts || {};
+  opts = opts || {}
 
   const d = await tmp.dir()
   const p = path.join(d.path, opts.package_name || 'unpm-testpackage')
@@ -173,7 +173,7 @@ const createTmpPackageDir = async (opts) => {
     console.log('created tmp package dir at %j', p)
   }
 
-  return p;
+  return p
 }
 
 const ensureTestPackage = async (pkgPath) => {
@@ -195,13 +195,13 @@ const ensureTestPackage = async (pkgPath) => {
  */
 const installLocalUnpmToPackage = async (pkgPath, opts) => {
   pkgPath = await ensureTestPackage(pkgPath)
-  const unpmRoot = appRoot;
+  const unpmRoot = appRoot
 
   const unpmPkg = await readPackageAsync(unpmRoot)
 
   await runPkgCmdAsync('npm pack', unpmRoot)
 
-  const unpmTarName = `${unpmPkg.name}-${unpmPkg.version}.tgz`;
+  const unpmTarName = `${unpmPkg.name}-${unpmPkg.version}.tgz`
   const unpmSourcePath = path.join(unpmRoot, unpmTarName)
   const unpmTargetDir = path.join(pkgPath, 'localpackage')
 
@@ -234,24 +234,24 @@ const installLocalUnpmToPackage = async (pkgPath, opts) => {
  */
 const installUnityPackageTemplateToTemp = async (opts) => {
 
-    opts = opts || {};
+    opts = opts || {}
 
     const d = await tmp.dir()
     const installPath = path.join(d.path, opts.package_name || 'unpm-testpackage')
 
     if(opts.verbose) {
-      mlog.pending(`will call unpm.unityPackage.installTemplate on install path '${installPath}' with opts ${JSON.stringify(opts)}...`);
+      mlog.pending(`will call unpm.unityPackage.installTemplate on install path '${installPath}' with opts ${JSON.stringify(opts)}...`)
     }
 
     await unpm.unityPackage.installTemplate(installPath, {...opts })
 
     if(opts.verbose) {
-      mlog.pending(`unpm.unityPackage.installTemplate completed on install path '${installPath}'...`);
+      mlog.pending(`unpm.unityPackage.installTemplate completed on install path '${installPath}'...`)
     }
 
     const installCmd =
         opts.run_npm_install_no_scripts? 'npm install --no-scripts':
-        opts.run_npm_install? 'npm install' : undefined;
+        opts.run_npm_install? 'npm install' : undefined
 
     if(!installCmd) { return installPath }
 
@@ -261,25 +261,98 @@ const installUnityPackageTemplateToTemp = async (opts) => {
 
 }
 
+
 /**
- * Many tests require a (tmp/test) unity project
- * that has unity-npm-utils installed (the local version we're testing)
- * as well as maybe some other package[s]
- *
- * @param {array} opts.install_packages array of packages to install
+ * @typedef {Object} NpmInstallPackageWithIgnoreScripts_Return
+ * @property {string} test_project_path path to the root of (the unity project) where the package was installed
+ * @property {string} test_package_name name of the package that was installed
+ * @property {object} test_package_scope scope of the package installed (e.g. a github account name)
+ * @property {object} test_package_full_name full string used to install the package, which is a combination of scope and package name, e.g. 'beatthat/placements'
+ * @property {string} test_package_expected_unity_install_path absolute path to where the package was installed (in the target unity project)
+ * @property {string} test_package_expected_unity_samples_path absolute path to where the package's 'Samples' were installed (in the target unity project)
  */
-// const createTmpUnityProjectWithPackagesInstalled = async(opts) => {
-//
-// }
+
+/**
+ * Install a package (to a unity-project root) with the ignore-scripts flag, e.g.
+ *
+ * <code>npm install --ignore-scripts $pkgScope/$pkgName</code>
+ *
+ * ...and returns a all the relevant path info.
+ *
+ * This is useful to test any scripts that run post install.
+ *
+ * @param {string} pkgName - the name of the package to install
+ * @param {string} pkgScope - the scope of the package to install (e.g. a github account name)
+ *
+ * @returns {NpmInstallPackageWithIgnoreScripts_Return} - an object containing path info about the install
+ */
+const npmInstallPackageWithIgnoreScripts = async function(pkgName, pkgScope)
+{
+    const unpmPkg = await unpm.readPackage(appRoot)
+
+    expect(unpmPkg.name).to.equal('unity-npm-utils')
+
+    //////////////////////////////////////////////////////////////////////
+    // first let's create a test package with unity-npm-utils installed...
+    //////////////////////////////////////////////////////////////////////
+
+    const testProjPath = await installLocalUnpmToPackage()
+
+    expect(
+      fs.existsSync(path.join(testProjPath, 'package.json')),
+      'test project should be installed at root ' + testProjPath
+    ).to.equal(true)
+
+    var testProj = await unpm.readPackage(testProjPath)
+
+    expect(
+      testProj.dependencies['unity-npm-utils']
+    ).to.exist
+
+    ////////////////////////////////////////////////////////////////////
+    // Now let's install a random unity package ('beatthat/properties' for this example)
+    // This is the package we will test against further down
+    ////////////////////////////////////////////////////////////////////
+
+    const testPkgFullName = `${pkgScope}/${pkgName}`
+
+    await runPkgCmdAsync('npm install --save --ignore-scripts ' + testPkgFullName, testProjPath)
+
+    testProj = await unpm.readPackage(testProjPath)
+
+    const testPkgInstallPath = path.join(testProjPath, 'Assets', 'Plugins', 'packages', pkgScope, pkgName)
+
+    expect(
+      await fs.existsAsync(testPkgInstallPath),
+      "(having installed package with ignore-scripts) the package should NOT yet be installed to unity"
+    ).to.equal(false)
+
+    const testPkgSamplesPath = path.join(testProjPath, 'Assets', 'Samples', 'packages', pkgScope, pkgName)
+
+    expect(
+      await fs.existsAsync(testPkgSamplesPath),
+      "(having installed package with ignore-scripts) the package's Samples should NOT yet be installed to unity"
+    ).to.equal(false)
+
+    return {
+        test_project_path: testProjPath,
+        test_package_name: pkgName,
+        test_package_scope: pkgScope,
+        test_package_full_name: testPkgFullName,
+        test_package_expected_unity_install_path: testPkgInstallPath,
+        test_package_expected_unity_samples_path: testPkgSamplesPath
+    }
+}
+
 
 const readPackageSync = (pkgPath) => {
     // don't use require because it will cache and we're here editting package.json
-    return JSON.parse(fs.readFileSync(path.join(pkgPath, 'package.json')));
+    return JSON.parse(fs.readFileSync(path.join(pkgPath, 'package.json')))
 }
 
 const readPackageAsync = async (pkgPath) => {
     // don't use require because it will cache and we're here editting package.json
-    return JSON.parse(await fs.readFileAsync(path.join(pkgPath, 'package.json')));
+    return JSON.parse(await fs.readFileAsync(path.join(pkgPath, 'package.json')))
 }
 
 exports.createTmpPackageDir = createTmpPackageDir
@@ -291,3 +364,4 @@ exports.installUnityPackageTemplateToTemp = installUnityPackageTemplateToTemp
 exports.installLocalUnpmToPackage = installLocalUnpmToPackage
 exports.readPackageSync = readPackageSync
 exports.readPackageAsync = readPackageAsync
+exports.npmInstallPackageWithIgnoreScripts = npmInstallPackageWithIgnoreScripts
