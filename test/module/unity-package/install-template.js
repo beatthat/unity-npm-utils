36a86c2d1a8a362b0cdc1c7699ce8d5a4b52b193
larrykirschner <larrykirschner@gmail.com>
2018-07-08 10:30:22 -0700

    passes unityPackage.installTemplate: updates sampledev package.json to depend on freshly built copy of the package

--- test/module/unity-package/install-template.js
+++ test/module/unity-package/install-template.js
@@ -5,13 +5,19 @@ const path = require('path')
 const h = require('../../test-helpers.js')
 const unpm = require('../../../lib/unity-npm-utils')
 
-describe("unityPackage.installTemplate - installs a the unity-package template to an empty directory", () => {
+const VERBOSE = true
+
+describe.only("unityPackage.installTemplate - installs a the unity-package template to an empty directory", () => {
     var pkgPath = null
 
     before(async function() {
         this.timeout(10000)
 
         pkgPath = await h.installUnityPackageTemplateToTemp()
+
+        if(VERBOSE) {
+            console.log(`installing at ${pkgPath}`)
+        }
     })
 
     it("installs template files", async () => {
@@ -29,6 +35,48 @@ describe("unityPackage.installTemplate - installs a the unity-package template t
     it("sets package name to install dir name by default", () => {
         const pkg = h.readPackageSync(pkgPath)
         expect(pkg.name).to.equal(path.basename(pkgPath))
+
+    })
+
+    it("updates test package.json to depend on freshly built copy of the package", async () => {
+
+        const pkg = h.readPackageSync(pkgPath)
+
+        const pkgTarFileName = `${pkg.name}-latest.tgz`
+
+        const testPkgPath = path.join(pkgPath, 'test', 'package.json')
+
+        expect(await fs.exists(testPkgPath),
+            `test package should exists at ${testPkgPath}`
+        ).to.equal(true)
+
+        const testPkg = h.readPackageSync(path.join(pkgPath, 'test'))
+
+
+        expect((testPkg.dependencies !== null && testPkg.dependencies[pkg.name] === `../${pkgTarFileName}`),
+            `test package should have dependency for ${pkg.name}`
+        ).to.equal(true)
+
+    })
+
+    it("updates sampledev package.json to depend on freshly built copy of the package", async () => {
+
+        const pkg = h.readPackageSync(pkgPath)
+
+        const pkgTarFileName = `${pkg.name}-latest.tgz`
+
+        const sampledevPkgPath = path.join(pkgPath, 'sampledev', 'package.json')
+
+        expect(await fs.exists(sampledevPkgPath),
+            `sampledev package should exists at ${sampledevPkgPath}`
+        ).to.equal(true)
+
+        const sampledevPkg = h.readPackageSync(path.join(pkgPath, 'sampledev'))
+
+        expect((sampledevPkg.dependencies !== null && sampledevPkg.dependencies[pkg.name] === `../${pkgTarFileName}`),
+            `sampledev package should have dependency for ${pkg.name} pointed to newly build tar`
+        ).to.equal(true)
+
     })
 
 })
