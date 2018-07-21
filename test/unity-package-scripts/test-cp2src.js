4b46c9b4d8aaf64dbf154d37c79b1f79535295af
Larry Kirschner <larrykirschner@gmail.com>
2018-07-21 04:25:12 -0700

    accidently committed 'only' on test

--- test/unity-package-scripts/test-cp2src.js
+++ test/unity-package-scripts/test-cp2src.js
@@ -3,7 +3,7 @@ const unpm = require('../../lib/unity-npm-utils');
 
 const cpTest2SrcBehaviour = require('../shared/package-cp-test2src-behaviour.js')
 
-describe.only(`'npm run test-cp2src' - copies files from a pkg's unity test proj back to src`, () => {
+describe(`'npm run test-cp2src' - copies files from a pkg's unity test proj back to src`, () => {
     cpTest2SrcBehaviour((opts) => {
         return h.runPkgCmd('npm run test-cp2src', opts.package_path);
     }, {
