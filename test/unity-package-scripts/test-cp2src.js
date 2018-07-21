c20bb2dbc074ff06da693d78db9e1a37d064f689
Larry Kirschner <larrykirschner@gmail.com>
2018-07-21 04:02:36 -0700

    renamed test scripts

--- test/unity-package-scripts/test-cp2src.js
+++ test/unity-package-scripts/test-cp2src.js
@@ -3,9 +3,9 @@ const unpm = require('../../lib/unity-npm-utils');
 
 const cpTest2SrcBehaviour = require('../shared/package-cp-test2src-behaviour.js')
 
-describe(`'npm run cp:test2src' - copies files from a pkg's unity test proj back to src`, () => {
+describe.only(`'npm run test-cp2src' - copies files from a pkg's unity test proj back to src`, () => {
     cpTest2SrcBehaviour((opts) => {
-        return h.runPkgCmd('npm run cp:test2src', opts.package_path);
+        return h.runPkgCmd('npm run test-cp2src', opts.package_path);
     }, {
         expect_deletes_from_package: false
     });
